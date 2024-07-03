2e2d37addacd9dc0936d2ebf7b7a38adc8450fd1
i.sakharov <i731901@gmail.com>
2024-07-03 16:37:42 +0300

    config added

---
+++ config.js
@@ -0,0 +1,12 @@
+import pkg from "pg";
+const { Pool } = pkg;
+
+const pool = new Pool({
+  user: "root",
+  host: "127.0.0.1",
+  database: "fortest",
+  password: "root",
+  port: "5432",
+});
+
+export { emailConfig, pool };
