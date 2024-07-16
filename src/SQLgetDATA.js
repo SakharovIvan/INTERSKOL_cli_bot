691c51c07b73cd64f0764a685a90c995bafb7547
i.sakharov <i731901@gmail.com>
2024-07-16 13:06:21 +0300

    search by SN

--- src/SQLgetDATA.js
+++ src/SQLgetDATA.js
@@ -9,14 +9,17 @@ const normalizeTlf = (tlf) => {
   //console.log(`+7 (${newtlf[1]}${newtlf[2]}${newtlf[3]}) ${newtlf[4]}${newtlf[5]}${newtlf[6]} ${newtlf[7]}${newtlf[8]} ${newtlf[9]}${newtlf[10]}`)
   return `+7 (${newtlf[1]}${newtlf[2]}${newtlf[3]}) ${newtlf[4]}${newtlf[5]}${newtlf[6]} ${newtlf[7]}${newtlf[8]} ${newtlf[9]}${newtlf[10]}`;
 };
-
+const normalizeSno = (sno)=>{
+  console.log(sno)
+ return sno.length===10?`0${sno}`:`00${sno}`
+}
 
 const getFulldataByTlf = async (tlf) => {
+  console.log(normalizeSno(tlf))
   const result = await pool.query(
-    `SELECT * FROM gis WHERE cli_telephone = '${normalizeTlf(tlf)}' ORDER BY id DESC;`
+    `SELECT * FROM gis WHERE cli_telephone = '${normalizeTlf(tlf)}' OR snNo_tool='${normalizeSno(tlf)}' ORDER BY id DESC;`
   );
-  //console.log(await result)
-  return result.rows[0];
+  return result;
 };
 //+7 (961) 120 73 22
 
