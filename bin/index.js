765ad6818381a3ea53555c502f1ee5e73e6d7445
i.sakharov <i731901@gmail.com>
2024-07-16 14:17:53 +0300

    good find

--- bin/index.js
+++ bin/index.js
@@ -1,10 +1,15 @@
 import { token } from "../config.js";
 import TelegramAPI from "node-telegram-bot-api";
-import getFulldataByTlf from "../src/SQLgetDATA.js";
+import { getFulldataByTlf, getFulldataBySno } from "../src/SQLgetDATA.js";
 const bot = new TelegramAPI(token, { polling: true });
 
 const textStart = `Добро пожаловать в телеграм бот ИНТЕРСКОЛ.\nЗдесь Вы можете проверить статус гарантийного ремонта по номеру телефона\nДля того, чтобы найти свой инструмент, введите номер телефона в следующем формате _7 999 999 99 99_`;
 const textMap = `В разработке, можете проверить ближайший асц по ссылке https://www.interskol.ru/centres \nИли отправить инструмент через Service online`;
+const textError_findTool = `Не удалось найти информаицю по Вашему телефону((\nДавайте попробуем поиск по серийному номеру в следующем формате 123.123456`;
+const textError_findSno = `Мы не нашли Вашего инструмента🥺, но не расстраивайтесь, попробуйте связаться с сервисным центром. Для поиска телефона сервисного центра введите команду /map`;
+
+const tlfFormat = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
+const sNoFormat = /\d{2,3}\.\d{6}/;
 const msgoption = {
   parse_mode: "Markdown",
   reply_markup: {
@@ -12,24 +17,24 @@ const msgoption = {
   },
 };
 
-const sentRepairInfo = async(chatID,result)=>{
+const sentRepairInfo = async (chatID, result) => {
   let dia =
-  (await result.date_dia) === ""
-    ? `\nИнструемнт еще не продиагностирован`
-    : `\nДата проведения диагнсотики: ${result.date_dia}`;
-let vip =
-  (await result.date_prin) === ""
-    ? `\nИнструемнт еще в ремонте`
-    : `\nДата выполнения ремонта: ${result.date_vipoln}`;
-console.log(result.date_prin,result.date_dia,result.date_vipoln)
-await bot.sendMessage(
-  chatID,
-  `🔫 Ваш инструмент: \nСерийный номер ${result.snno_tool}\nКод машины ${result.matno_tool} \nСервисный центр ${result.asc_name} \nВид ремонта ${result.vr}\n
+    (await result.date_dia) === ""
+      ? `\nИнструемнт еще не продиагностирован`
+      : `\nДата проведения диагнсотики: ${result.date_dia}`;
+  let vip =
+    (await result.date_prin) === ""
+      ? `\nИнструемнт еще в ремонте`
+      : `\nДата выполнения ремонта: ${result.date_vipoln}`;
+      
+  await bot.sendMessage(
+    chatID,
+    `🔫 Ваш инструмент: \nСерийный номер ${result.snno_tool}\nКод машины ${result.matno_tool} \nСервисный центр ${result.asc_name} \nВид ремонта ${result.vr}\n
 ⚒️ Статус ремонта: \nДата принято: ${result.date_prin}${dia}${vip}\n
 🧰 Для связи с АСЦ: \nТелефон АСЦ ${result.asc_telephone} \nАдрес АСЦ ${result.asc_adr}`,
-  msgoption
-);
-}
+    msgoption
+  );
+};
 
 const start = async () => {
   bot.setMyCommands([
@@ -44,21 +49,37 @@ const start = async () => {
     const time = msg.date;
     try {
       switch (true) {
-        case text==="/start":
+        case text === "/start":
           await bot.sendMessage(chatID, textStart, msgoption);
           break;
-        case text==="/map":
+        case text === "/map":
           await bot.sendMessage(chatID, textMap, msgoption);
           break;
-        case text==="/findbytelephonenumber":
+        case text === "/findbytelephonenumber":
           await bot.sendMessage(chatID, textStart, msgoption);
           break;
-        default:
+
+        case tlfFormat.test(text):
           const fulldata = await getFulldataByTlf(text);
-          const result = await fulldata.rows[0];
-          await sentRepairInfo(chatID,result)
+          if (fulldata.rows.length > 0) {
+            const result = await fulldata.rows[0];
+            await sentRepairInfo(chatID, result);
+          } else {
+            await bot.sendMessage(chatID, textError_findTool, msgoption);
+          }
+          break;
 
+        case sNoFormat.test(text):
+          const fulldataSno = await getFulldataBySno(text);
+          if (fulldataSno.rows.length > 0) {
+            const resultSno = await fulldataSno.rows[0];
+            await sentRepairInfo(chatID, resultSno);
+          } else {
+            await bot.sendMessage(chatID, textError_findSno, msgoption);
+          }
+          break;
 
+        default:
           break;
       }
     } catch (err) {
--- src/SQLgetDATA.js
+++ src/SQLgetDATA.js
@@ -9,22 +9,26 @@ const normalizeTlf = (tlf) => {
   //console.log(`+7 (${newtlf[1]}${newtlf[2]}${newtlf[3]}) ${newtlf[4]}${newtlf[5]}${newtlf[6]} ${newtlf[7]}${newtlf[8]} ${newtlf[9]}${newtlf[10]}`)
   return `+7 (${newtlf[1]}${newtlf[2]}${newtlf[3]}) ${newtlf[4]}${newtlf[5]}${newtlf[6]} ${newtlf[7]}${newtlf[8]} ${newtlf[9]}${newtlf[10]}`;
 };
-const normalizeSno = (sno)=>{
-  console.log(sno)
- return sno.length===10?`0${sno}`:`00${sno}`
-}
+
+const normalizeSno = (sno) => {
+  console.log(sno);
+  return sno.length === 10 ? `0${sno}` : `00${sno}`;
+};
 
 const getFulldataByTlf = async (tlf) => {
-  console.log(normalizeSno(tlf))
   const result = await pool.query(
-    `SELECT * FROM gis WHERE cli_telephone = '${normalizeTlf(tlf)}' OR snNo_tool='${normalizeSno(tlf)}' ORDER BY id DESC;`
+    `SELECT * FROM gis WHERE cli_telephone = '${normalizeTlf(
+      tlf
+    )}' ORDER BY id DESC;`
   );
   return result;
 };
-//+7 (961) 120 73 22
 
+const getFulldataBySno = async (sno) => {
+  const result = await pool.query(
+    `SELECT * FROM gis WHERE snNo_tool='${normalizeSno(sno)}' ORDER BY id DESC;`
+  );
+  return result;
+};
 
-
-
-
-export default getFulldataByTlf;
+export { getFulldataByTlf, getFulldataBySno };
