87e5ca0dff6d91baa87c71f5b95e8f8aa3d8b007
i.sakharov <i731901@gmail.com>
2024-08-20 13:16:18 +0300

    botlog

--- src/botlogSQL.js
+++ src/botlogSQL.js
@@ -2,7 +2,7 @@ import { pool } from "../config.js";
 
 export default async(chatID, clie, text, time)=>{
     try{
-        await pool.query(`INSERT INTO clientlog (chatid, cli, text,time) VALUES (${chatID},'${clie}','${text}',${time} );`)
+        await pool.query(`INSERT INTO clientlog (chatid, cli, text,time) VALUES (${chatID},'${clie}','${text}',${Number(time)} );`)
         console.log('Cli Log added')
     }catch(err){console.log(err)}
     }
