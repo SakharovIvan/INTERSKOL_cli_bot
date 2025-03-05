1b08710fc85138a06c46af6d430aa9afaa660703
SakharovIvan <i731901@gmail.com>
2025-03-05 23:16:28 +0300

    05032025

--- bin/index.js
+++ bin/index.js
@@ -45,7 +45,7 @@ const sentRepairInfo = async (chatID, result) => {
     chatID,
     `🔫 Ваш инструмент: \nСерийный номер ${result?.snno_tool}\nКод машины ${result?.matno_tool} \nСервисный центр ${result?.asc_name} \nВид ремонта ${result?.vr}\n
 ⚒️ Статус ремонта: \nДата принято: ${result?.date_prin}${dia}${vip}\n
-🧰 Для связи с АСЦ: \nТелефон АСЦ ${result?.asc_telephone} \nАдрес АСЦ ${result.asc_adr}`,
+🧰 Для связи с АСЦ: \nТелефон АСЦ ${result?.asc_telephone} \nАдрес АСЦ ${result?.asc_adr}`,
     msgoption
   );
 };
